6629582057bdc290b823f79b20d8aa8bb89cdc8e
David Bergan <dbergan@gmail.com>
2023-08-09 22:59:05 -0500

    BETTER TRADE ROUTES - Remove minimum time length

--- DB - Civ 6 Multiplayer Gold/BetterTradeRoutes/DB_BetterTradeRoutes.sql
+++ DB - Civ 6 Multiplayer Gold/BetterTradeRoutes/DB_BetterTradeRoutes.sql
@@ -1,3 +1,6 @@
+-- Trade routes have no minimum distance
+UPDATE GlobalParameters SET Value = 0 WHERE Name = 'TRADE_ROUTE_TURN_DURATION_BASE' ;
+
 -- International route yields match domestic
 UPDATE District_TradeRouteYields SET YieldChangeAsInternationalDestination = YieldChangeAsDomesticDestination WHERE YieldChangeAsDomesticDestination > YieldChangeAsInternationalDestination ;
 
